d1addddb1c8afae19d12177ee487ee13788ca88a
Henry F Ma <henryma@edening.cn>
2019-03-17 18:49:09 +0800

    Expose IPFS config

--- src/node.js
+++ src/node.js
@@ -2,12 +2,26 @@
 
 const IPFS = require('ipfs')
 
+const defaults = {
+      init: true,
+      start: true,
+      EXPERIMENTAL: {},
+      preload: {
+        enabled: false,
+        addresses: [
+          '/dnsaddr/node0.preload.ipfs.io/https',
+          '/dnsaddr/node1.preload.ipfs.io/https'
+        ]
+      }
+}
+
 let node
 
 /* start a IPFS node within the service worker */
-const startNode = () => {
+const startNode = (configs) => {
   return new Promise((resolve) => {
-    node = new IPFS()
+    //node = new IPFS()
+    node = new IPFS(configs || defaults)
     node.on('error', (error) => {
       console.log(error.toString())
     })
@@ -19,10 +33,10 @@ const startNode = () => {
 }
 
 /* get a ready to use IPFS node */
-const getNode = () => {
+const getNode = (configs) => {
   return new Promise((resolve) => {
     if (!node) {
-      return startNode().then((node) => resolve(node))
+      return startNode(configs || defaults).then((node) => resolve(node))
     }
 
     resolve(node)
